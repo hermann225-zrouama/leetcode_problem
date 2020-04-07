a012e576d4086fb382717368cf76dd5f7cfd3d86
hermann225-zrouama <62513048+hermann225-zrouama@users.noreply.github.com>
2020-04-07 09:44:14 +0000

    Create Populating Next Right Pointers in Each Node.py

---
+++ Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        leftnode = root
+        while leftnode and leftnode.left != None:
+            iterateur = leftnode
+            while(iterateur != None):
+                iterateur.left.next = iterateur.right
+                if iterateur.next != None:
+                    iterateur.right.next = iterateur.next.left
+                iterateur = iterateur.next
+            leftnode = leftnode.left
+        return root
+    
+        
+            
